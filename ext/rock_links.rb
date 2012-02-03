ef7f9ff3623ba0fbf050294f62000cbfdd56328a
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-02-03 16:53:39 +0100

    add the rock_source tag

--- ext/rock_links.rb
+++ ext/rock_links.rb
@@ -36,6 +36,25 @@ class RockTicket
     end
 end
 
+class RockSource
+    include Webgen::Tag::Base
+
+    def call(tag, body, context)
+        uri = param('rocksource.url')
+        parts = uri.split('/')
+
+        url = [parts.shift]
+        if parts.first == "orogen"
+            parts.shift
+            url << "orogen-#{parts.shift}"
+        else
+            url << parts.shift
+        end
+        url << "blobs" << "master"
+        "http://gitorious.org/#{url.join("/")}"
+    end
+end
+
 config = Webgen::WebsiteAccess.website.config
 config.rocktype.name nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_type'] = 'RockType'
@@ -45,4 +64,6 @@ config.rocktask.name nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_task'] = 'RockTask'
 config.rockticket.ticket_id 0, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_ticket'] = 'RockTicket'
+config.rocksource.url nil, :mandatory => 'default'
+config['contentprocessor.tags.map']['rock_source'] = 'RockSource'
 
