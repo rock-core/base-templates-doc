d74e9446f5641b531957a311e1676316fe4b7676
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-02-03 15:07:59 +0100

    add rock_ticket to the list of rock-specific tags

--- ext/rock_links.rb
+++ ext/rock_links.rb
@@ -27,6 +27,15 @@ class RockTask
     end
 end
 
+class RockTicket
+    include Webgen::Tag::Base
+
+    def call(tag, body, context)
+        ticket_id = param('rockticket.ticket_id')
+        "http://rock.opendfki.de/ticket/#{id}"
+    end
+end
+
 config = Webgen::WebsiteAccess.website.config
 config.rocktype.name nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_type'] = 'RockType'
@@ -34,4 +43,6 @@ config.rockpackage.name nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_pkg'] = 'RockPackage'
 config.rocktask.name nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_task'] = 'RockTask'
+config.rockticket.ticket_id 0, :mandatory => 'default'
+config['contentprocessor.tags.map']['rock_ticket'] = 'RockTicket'
 
